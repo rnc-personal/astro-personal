b2239d0e8756bbb0c8e72ca0921c04ef800220d0
rcruk <richard.challenger@rubiesuk.com>
2024-01-21 11:42:28 +0000

    Moved Components over and Removed TinaCMS refs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
 import vue from "@astrojs/vue";
 
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [
+    tailwind(),
+    vue(),
+    storyblok(
+      {
+        accessToken: env.STORYBLOK_TOKEN,
+        components: {
+          // Add your components here
+        },
+        apiOptions: {
+          region: 'eu',
+        },
+      }
+    )]
 });
