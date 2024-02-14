8640464d17ab9ce3e0e5350b673c519b1be18928
rcruk <richard.challenger@rubiesuk.com>
2024-02-14 19:38:28 +0000

    Add Custom Grid Layout for CV Entries

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				'cv-grid': '1fr 32px 1fr',
+			},
+		},
 		screens: {
 			'sm': '480px',
 			'md': '768px',
