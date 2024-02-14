a69eb5750f6923faa84cd9c04dcffeb0ed640ae7
rcruk <richard.challenger@rubiesuk.com>
2024-02-14 13:03:17 +0000

    FMade Nav All Vue, checks for menu open/close on click

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,19 +1,44 @@
 <template>
-  <div>
-    <div class="nav-menu mobile-nav">
-      <div class="toggle" @click="toggleDropdown">
-        <svg v-if="!showDropdown" class="plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M11 11V4a1 1 0 0 1 2 0v7h7a1 1 0 0 1 0 2h-7v7a1 1 0 0 1-2 0v-7H4a1 1 0 0 1 0-2h7z" stroke-width="4"
-            stroke-linecap="square" />
-        </svg>
-        <svg v-else class="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M19 13H5v-2h14v2z" />
-        </svg>
+  <section class="bg-gray-950">
+    <div class="container xl:px-40 lg:px-32 md:px-16 sm:px-8 px-4 mx-auto flex items-center sm: justify-around">
+      <a @click="showDropdown = false" href="/">
+        <h1
+          class="text-7xl leading-snug py-1 selection:font-black text-gray-50 hover:text-yellow-500 transition-colors ease-in-out">
+          HEADING
+        </h1>
+      </a>
+
+
+      <div class="nav-menu mobile-nav">
+        <div class="toggle" @click="toggleDropdown">
+          <svg v-if="!showDropdown" class="plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M11 11V4a1 1 0 0 1 2 0v7h7a1 1 0 0 1 0 2h-7v7a1 1 0 0 1-2 0v-7H4a1 1 0 0 1 0-2h7z" stroke-width="4"
+              stroke-linecap="square" />
+          </svg>
+          <svg v-else class="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 13H5v-2h14v2z" />
+          </svg>
+        </div>
+        <div ref="navigation" class="dropdown-mob py-2 px-6" :class="{ active: showDropdown }">
+          <div class="nav-inner__mob">
+            <ul class="flex gap-2 sm:flex-col md:flex-row">
+              <a @click="showDropdown = false" href="/profile">
+                <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Profile</li>
+              </a>
+              <a @click="showDropdown = false" href="/projects">
+                <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Projects</li>
+              </a>
+              <a @click="showDropdown = false" href="/contact">
+                <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Contact</li>
+              </a>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div ref="navigation" class="dropdown-mob py-2 px-6" :class="{ active: showDropdown }">
-        <div class="nav-inner__mob">
+      <div class="nav-menu desktop-nav">
+        <div class="nav-inner">
           <ul class="flex gap-2 sm:flex-col md:flex-row">
             <a href="/profile">
               <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Profile</li>
@@ -28,22 +53,7 @@
         </div>
       </div>
     </div>
-    <div class="nav-menu desktop-nav">
-      <div class="nav-inner">
-        <ul class="flex gap-2 sm:flex-col md:flex-row">
-          <a href="/profile">
-            <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Profile</li>
-          </a>
-          <a href="/projects">
-            <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Projects</li>
-          </a>
-          <a href="/contact">
-            <li class="text-2xl text-gray-50 hover:text-yellow-500 uppercase">Contact</li>
-          </a>
-        </ul>
-      </div>
-    </div>
-  </div>
+  </section>
 </template>
 <script>
 import { ref } from "vue";
