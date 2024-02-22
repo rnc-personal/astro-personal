11ada7b3d28b9c795fd3a2a380e1a0351c078f4b
rcruk <richard.challenger@rubiesuk.com>
2024-02-22 14:03:28 +0000

    Added Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,43 +2,41 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from "@astrojs/vue";
-
+import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
-  integrations: [
-    tailwind(),
-    vue(),
-    storyblok({
-        accessToken: env.STORYBLOK_TOKEN,
-        components: {
-          page: 'storyblok/components/Page',
-          profileheader: 'storyblok/components/Profile',
-          imagegallery: 'storyblok/components/ImageGallery',
-          projectlisting: 'storyblok/components/ProjectListing',
-          projectcard: 'storyblok/components/ProjectCard', 
-          projectdetail: 'storyblok/components/ProjectDetail',
-          cventry: 'storyblok/components/CvEntry',
-          cv_task_block: 'storyblok/components/CVTaskBlock',
-          profile_skills: 'storyblok/components/ProfileSkills',
-          profile_skill_block: 'storyblok/components/ProfileSkillBlock',
-          profile_skill_detail: 'storyblok/components/SkillDetailBlock',
-          contact_card: 'storyblok/components/ContactCard',
-          contact_services_block: 'storyblok/components/ContactServicesBlock',
-        },
-        apiOptions: {
-          region: 'eu',
-        },
-      }
-    )]
+  integrations: [tailwind(), vue(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/components/Page',
+      profileheader: 'storyblok/components/Profile',
+      imagegallery: 'storyblok/components/ImageGallery',
+      projectlisting: 'storyblok/components/ProjectListing',
+      projectcard: 'storyblok/components/ProjectCard',
+      projectdetail: 'storyblok/components/ProjectDetail',
+      cventry: 'storyblok/components/CvEntry',
+      cv_task_block: 'storyblok/components/CVTaskBlock',
+      profile_skills: 'storyblok/components/ProfileSkills',
+      profile_skill_block: 'storyblok/components/ProfileSkillBlock',
+      profile_skill_detail: 'storyblok/components/SkillDetailBlock',
+      contact_card: 'storyblok/components/ContactCard',
+      contact_services_block: 'storyblok/components/ContactServicesBlock'
+    },
+    apiOptions: {
+      region: 'eu'
+    }
+  })],
+  output: "server",
+  adapter: vercel()
 });
